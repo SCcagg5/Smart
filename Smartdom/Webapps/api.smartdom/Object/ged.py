8285ded588fd6bb7dfa5b68eeea12e93ac1bc567
SCcagg5 <eliot@LAPTOP-C7G3T243.localdomain>
2020-07-27 19:44:51 +0200

    update sharing

--- Smartdom/Webapps/api.smartdom/Object/ged.py
+++ Smartdom/Webapps/api.smartdom/Object/ged.py
@@ -15,6 +15,8 @@ class folder:
         date = str(int(round(time.time() * 1000)))
         if folder_id is not None and not self.exist(folder_id):
             return [False, "folder_id does not exist", 400]
+        if not self.is_proprietary(folder_id):
+            return [False, "Invalid rights", 403]
         succes = sql.input("INSERT INTO `folder` (`id`,`user_id`, `name`, `inside`, `date`) VALUES (%s, %s, %s, %s, %s)", \
         (id, self.usr_id, name, folder_id, date))
         if not succes:
@@ -22,12 +24,16 @@ class folder:
         return [True, {}, None]
 
     def share(self, email, folder_id, access):
-        if not self.is_proprietary(folder_id):
-            return [False, "Can't share this folder, you're not proprietary or moderator", 403]
+        if not self.is_proprietary(folder_id) and not self.is_sharer(folder_id):
+            return [False, "Can't share this file, invalid rights", 403]
         if "administrate" not in access or "share" not in access or "edit" not in access or "read" not in access:
             return [False, "Invalid access, should contain 'share', 'edit', 'read', 'administrate'", 400]
         if not isinstance(access["administrate"], bool) or not isinstance(access["edit"], bool) or not isinstance(access["read"], bool) or not isinstance(access["administrate"], bool):
             return [False, "Value of index contained in access should be boolean", 400]
+        if access["administrate"]:
+            access["edit"] = True
+        if access["edit"]:
+            acces["read"] = true
         user_id = user.fromemail(email)
         table = "share_folder"
         if not user_id:
@@ -37,8 +43,9 @@ class folder:
             return [False, "Can't share to yourself", 401]
         share_id = str(uuid.uuid4())
         date = str(int(round(time.time() * 1000)))
-        succes = sql.input("INSERT INTO `" + table + "` (`id`,`user_id`, `folder_id`, `can_administrate`, `can_share`, `can_edit`, `can_read`, `date`, `shared_by`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", \
-        (share_id, user_id, folder_id, access["administrate"], access["share"], access["edit"], access["read"], date, self.usr_id))
+        sql.input("UPDATE  `" + table + "` SET active = 0 WHERE user_id = %s AND folder_id = %s", (user_id, folder_id))
+        succes = sql.input("INSERT INTO `" + table + "` (`id`,`user_id`, `folder_id`, `can_administrate`, `can_share`, `can_edit`, `can_read`, `date`, `shared_by`, `active`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", \
+        (share_id, user_id, folder_id, access["administrate"], access["share"], access["edit"], access["read"], date, self.usr_id, True))
         if not succes:
             return [False, "data input error", 500]
         return [True, {}, None]
@@ -48,15 +55,67 @@ class folder:
         (folder_id, ))
         return True if len(res) > 0 else False
 
-    def is_proprietary(self, id_folder):
-        res = sql.get("SELECT `id`, `name`, `date`, `inside` FROM `folder` WHERE id = %s AND user_id = %s", (id_folder, self.usr_id))
-        return True if len(res) > 0 else False
-
-    def content(self, folder_id = None, name = None, date = None, i = 5):
+    def sharedcontent(self, folder_id = None, name = None, date = None):
         res = {"id": folder_id, "name": name, "date": date, "Content": {"files": [], "folders": []}}
-        i -= 1
-        if i < 0:
-            return res
+        if folder_id is not None:
+            res["rights"] = {
+                             "read": self.is_reader(folder_id),
+                             "edit": self.is_editor(folder_id),
+                             "share": self.is_sharer(folder_id),
+                             "administrate": self.is_admin(folder_id)
+                             }
+            if not self.is_reader(folder_id)
+                return res
+            files = sql.get("SELECT `id`, `name`, `type`, `date` FROM `file` WHERE inside = %s", \
+            (folder_id, ))
+            for i2 in files:
+                res["Content"]["files"].append({
+                "id": i2[0], "name": i2[1], "type": i2[2], "date": i2[3],
+                "rights": {
+                        "read": file(self.usr_id).is_reader(i2[0]),
+                        "edit": file(self.usr_id).is_editor(i2[0]),
+                        "share": file(self.usr_id).is_sharer(i2[0]),
+                        "administrate": file(self.usr_id).is_admin(i2[0])
+                        }
+                })
+            folders = sql.get("SELECT `id`, `name`, `date` FROM `folder` WHERE inside = %s", \
+            (folder_id, ))
+            for i2 in folders:
+                res["Content"]["folders"].append(self.sharedcontent(i2[0], i2[1], i2[2]))
+        else:
+            files = sql.get("SELECT file.`id`, `name`, file.`date`, user.`email`, share_file.`date` FROM `file` INNER JOIN `share_file` ON `share_file`.user_id = %s LEFT JOIN `user` ON user.id = %s WHERE valid IS TRUE", \
+            (self.usr_id, self.usr_id))
+            for i2 in files:
+                res["Content"]["files"].append({
+                "id": i2[0], "name": i2[1], "type": i2[2], "date": i2[3], "proprietary": i2[4], "sharing_date": i2[5],
+                "rights": {
+                        "read": file(self.usr_id).is_reader(i2[0]),
+                        "edit": file(self.usr_id).is_editor(i2[0]),
+                        "share": file(self.usr_id).is_sharer(i2[0]),
+                        "administrate": file(self.usr_id).is_admin(i2[0])
+                        }
+                })
+            folders = sql.get("SELECT folder.`id`, `name`, folder.`date`, user.`email`, share_folder.`date` FROM `folder` INNER JOIN `share_folder` ON `share_folder`.user_id = %s LEFT JOIN `user` ON user.id = %s WHERE valid IS TRUE", \
+            (self.usr_id, self.usr_id))
+            for i2 in folders:
+                res["Content"]["folders"].append({
+                "id": i2[0], "name": i2[1], "date": i2[2], "proprietary": i2[3], "sharing_date": i2[4],
+                "rights": {
+                            "read": self.is_reader(i2[0]),
+                            "edit": self.is_editor(i2[0]),
+                            "share": self.is_sharer(i2[0]),
+                            "administrate": self.is_admin(i2[0])
+                          }
+                })
+        return res
+
+    def content(self, folder_id = None, name = None, date = None):
+        res = {"id": folder_id, "name": name, "date": date, "Content": {"files": [], "folders": []}}
+        if folder_id is not None and name is None:
+            folder = sql.get("SELECT `name`, `date` FROM `folder` WHERE id = %s AND user_id = %s", \
+            (folder_id, self.usr_id))
+            res["name"] = res[0]
+            res["date"] = res[1]
         if folder_id is not None:
             files = sql.get("SELECT `id`, `name`, `type`, `date` FROM `file` WHERE inside = %s AND user_id = %s", \
             (folder_id, self.usr_id))
@@ -74,7 +133,56 @@ class folder:
             folders = sql.get("SELECT `id`, `name`, `date` FROM `folder` WHERE inside IS NULL AND user_id = %s", \
             (self.usr_id, ))
         for i2 in folders:
-            res["Content"]["folders"].append(self.content(i2[0], i2[1], i2[2], i))
+            res["Content"]["folders"].append(self.content(i2[0], i2[1], i2[2]))
+        return res
+
+    def is_proprietary(self, id_folder, user_id = None):
+        user_id = self.usr_id if user_id is None else user_id
+        res = sql.get("SELECT `id` FROM `folder` WHERE id = %s AND user_id = %s", (id_folder, user_id))
+        return True if len(res) > 0 else False
+
+    def is_admin(self, id_folder):
+        res = False
+        for i in ged.vpath(id_folder):
+            res = sql.get("SELECT `id` FROM `share_folder` WHERE folder_id = %s AND user_id = %s AND can_administrate IS TRUE AND active IS TRUE", (i, self.usr_id))
+            if len(res):
+                res = True
+            res = sql.get("SELECT `id` FROM `share_folder` WHERE folder_id = %s AND user_id = %s AND can_administrate IS FALSE AND active IS TRUE", (i, self.usr_id))
+            if len(res):
+                res = False
+        return res
+
+    def is_sharer(self, id_folder):
+        res = False
+        for i in ged.vpath(id_folder):
+            res = sql.get("SELECT `id` FROM `share_folder` WHERE folder_id = %s AND user_id = %s AND can_share IS TRUE AND active IS TRUE", (i, self.usr_id))
+            if len(res):
+                res = True
+            res = sql.get("SELECT `id` FROM `share_folder` WHERE folder_id = %s AND user_id = %s AND can_share IS FALSE AND active IS TRUE", (i, self.usr_id))
+            if len(res):
+                res = False
+        return res
+
+    def is_editor(self, id_folder):
+        res = False
+        for i in ged.vpath(id_folder):
+            res = sql.get("SELECT `id` FROM `share_folder` WHERE folder_id = %s AND user_id = %s AND can_edit IS TRUE AND active IS TRUE", (i, self.usr_id))
+            if len(res):
+                res = True
+            res = sql.get("SELECT `id` FROM `share_folder` WHERE folder_id = %s AND user_id = %s AND can_edit IS FALSE AND active IS TRUE", (i, self.usr_id))
+            if len(res):
+                res = False
+        return res
+
+    def is_reader(self, id_folder):
+        res = False
+        for i in ged.vpath(id_folder):
+            res = sql.get("SELECT `id` FROM `share_folder` WHERE folder_id = %s AND user_id = %s AND can_read IS TRUE AND active IS TRUE", (i, self.usr_id))
+            if len(res):
+                res = True
+            res = sql.get("SELECT `id` FROM `share_folder` WHERE folder_id = %s AND user_id = %s AND can_read IS FALSE AND active IS TRUE", (i, self.usr_id))
+            if len(res):
+                res = False
         return res
 
 class file:
@@ -89,6 +197,8 @@ class file:
             return [False, "File extension not allowed.", 401]
         if folder_id is not None and not folder(self.usr_id).exist(folder_id):
             return [False, "folder_id does not exist", 400]
+        if not self.is_proprietary(folder_id):
+            return [False, "Invalid rights", 403]
         file.save(self.path(file_id))
         succes = sql.input("INSERT INTO `file` (`id`,`user_id`, `name`, `type`, `inside`, `date`) VALUES (%s, %s, %s, %s, %s, %s)", \
         (file_id, self.usr_id, name, ext[1:], folder_id, date))
@@ -97,12 +207,16 @@ class file:
         return [True, {"file_id": file_id}, None]
 
     def share(self, email, file_id, access):
-        if not self.is_proprietary(file_id):
-            return [False, "Can't share this file, you're not proprietary or moderator", 403]
+        if not self.is_proprietary(file_id) and not self.is_sharer(file_id):
+            return [False, "Can't share this file, invalid rights", 403]
         if "administrate" not in access or "share" not in access or "edit" not in access or "read" not in access:
             return [False, "Invalid access, should contain 'share', 'edit', 'read', 'administrate'", 400]
         if not isinstance(access["administrate"], bool) or not isinstance(access["edit"], bool) or not isinstance(access["read"], bool) or not isinstance(access["administrate"], bool):
             return [False, "Value of index contained in access should be boolean", 400]
+        if access["administrate"]:
+            access["edit"] = True
+        if access["edit"]:
+            acces["read"] = true
         user_id = user.fromemail(email)
         table = "share_file"
         if not user_id:
@@ -112,8 +226,9 @@ class file:
             return [False, "Can't share to yourself", 401]
         share_id = str(uuid.uuid4())
         date = str(int(round(time.time() * 1000)))
-        succes = sql.input("INSERT INTO `" + table + "` (`id`,`user_id`, `file_id`, `can_administrate`, `can_share`, `can_edit`, `can_read`, `date`, `shared_by`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", \
-        (share_id, user_id, file_id, access["administrate"], access["share"], access["edit"], access["read"], date, self.usr_id))
+        sql.input("UPDATE  `" + table + "` SET active = 0 WHERE user_id = %s AND file_id = %s", (user_id, file_id))
+        succes = sql.input("INSERT INTO `" + table + "` (`id`,`user_id`, `file_id`, `can_administrate`, `can_share`, `can_edit`, `can_read`, `date`, `shared_by`, active) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", \
+        (share_id, user_id, file_id, access["administrate"], access["share"], access["edit"], access["read"], date, self.usr_id, True))
         if not succes:
             return [False, "data input error", 500]
         return [True, {}, None]
@@ -134,7 +249,8 @@ class file:
 
     def content(self, file_id):
         ret = None
-        #temp
+        if not self.is_proprietary(file_id) and not self.is_reader(file_id):
+            return [False, "Invalid rights", 403]
         res = sql.get("SELECT `id`, `name`, `date`, `type`, `inside` FROM `file` WHERE id = %s AND user_id = %s",
         (file_id, self.usr_id,))
         if len(res) != 0:
@@ -153,18 +269,83 @@ class file:
                    }
         return ret
 
-    def is_proprietary(self, id_file):
-        res = sql.get("SELECT `id`, `name`, `date`, `type`, `inside` FROM `file` WHERE id = %s AND user_id = %s", (id_file, self.usr_id))
+    def is_proprietary(self, id_file, user_id = None):
+        user_id = self.usr_id if user_id is None else user_id
+        res = sql.get("SELECT `id` FROM `file` WHERE id = %s AND user_id = %s", (id_file, user_id))
         return True if len(res) > 0 else False
 
+    def is_admin(self, id_file):
+        res = False
+        for i in ged.vpath(id_file):
+            res = sql.get("SELECT `id` FROM `share_file` WHERE file_id = %s AND user_id = %s AND can_administrate IS TRUE AND active IS TRUE", (i, self.usr_id))
+            if len(res):
+                res = True
+            res = sql.get("SELECT `id` FROM `share_file` WHERE file_id = %s AND user_id = %s AND can_administrate IS FALSE AND active IS TRUE", (i, self.usr_id))
+            if len(res):
+                res = False
+        return res
+
+    def is_sharer(self, id_file):
+        res = False
+        for i in ged.vpath(id_file):
+            res = sql.get("SELECT `id` FROM `share_file` WHERE file_id = %s AND user_id = %s AND can_share IS TRUE AND active IS TRUE", (i, self.usr_id))
+            if len(res):
+                res = True
+            res = sql.get("SELECT `id` FROM `share_file` WHERE file_id = %s AND user_id = %s AND can_share IS FALSE AND active IS TRUE", (i, self.usr_id))
+            if len(res):
+                res = False
+        return res
+
+    def is_editor(self, id_file):
+        res = False
+        for i in ged.vpath(id_file):
+            res = sql.get("SELECT `id` FROM `share_file` WHERE file_id = %s AND user_id = %s AND can_edit IS TRUE AND active IS TRUE", (i, self.usr_id))
+            if len(res):
+                res = True
+            res = sql.get("SELECT `id` FROM `share_file` WHERE file_id = %s AND user_id = %s AND can_edit IS FALSE AND active IS TRUE", (i, self.usr_id))
+            if len(res):
+                res = False
+        return res
+
+    def is_reader(self, id_file):
+        res = False
+        for i in ged.vpath(id_file):
+            res = sql.get("SELECT `id` FROM `share_file` WHERE file_id = %s AND user_id = %s AND can_read IS TRUE AND active IS TRUE", (i, self.usr_id))
+            if len(res):
+                res = True
+            res = sql.get("SELECT `id` FROM `share_file` WHERE file_id = %s AND user_id = %s AND can_read IS FALSE AND active IS TRUE", (i, self.usr_id))
+            if len(res):
+                res = False
+        return res
+
+
 class ged:
+
+    def vpath(id_doc):
+        res = []
+        if not self.exist(id_doc):
+            id_doc = sql.get("SELECT `inside` FROM `file` WHERE id = %s", (id_doc, ))
+        while id_doc is not None:
+            res.append(id_doc)
+            id_doc = sql.get("SELECT `inside` FROM `folder` WHERE id = %s", (id_doc, ))
+        return res
+
+    def is_inside(id_doc, id_folder):
+        path = ged.vpath(id_doc)[:-1]
+        return id_folder in path
+
     def __init__(self, usr_id = -1):
         self.usr_id = str(usr_id)
 
     def get(self, doc_id):
         ret = None
-        if doc_id is None or folder(self.usr_id).exist(doc_id):
-            ret = folder(self.usr_id).content(doc_id)
+        if doc_id is None:
+            ret = folder(self.usr_id).content(doc_id) + folder(self.usr_id).sharedcontent(doc_id)
+        elif folder(self.usr_id).exist(doc_id):
+            if folder(self.usr_id).is_proprietary(doc_id):
+                ret = folder(self.usr_id).content(doc_id)
+            else:
+                ret = folder(self.usr_id).sharedcontent(doc_id)
         elif file(self.usr_id).exist(doc_id):
             ret = file(self.usr_id).content(doc_id)
         return [True, ret, None]
