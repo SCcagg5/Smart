aa3b643e7002f490cdef29feb891927a82dedf70
amine babba <ababba@toolynk.com>
2020-10-21 14:06:47 +0100

    upadte brainy**

---
+++ Smartco/Webapps/ged.brainyfood/src/provider/patientservice.js
@@ -0,0 +1,69 @@
+
+const endpoint = process.env.REACT_APP_endpoint
+
+let  PatientService = {
+
+    loadHeaders() {
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/x-www-form-urlencoded;charset=UTF-8');
+        headers.append("Accept", '*/*');
+        return headers;
+    },
+
+
+    CreatePatient(data) {
+        let formBody = [];
+        for (let property in data) {
+            let encodedKey = encodeURIComponent(property);
+            let encodedValue = encodeURIComponent(data[property]);
+            formBody.push(encodedKey + "=" + encodedValue);
+        }
+        formBody = formBody.join("&");
+        return fetch(endpoint + 'PatientCreate', {
+            method: 'POST',
+            headers: this.loadHeaders(),
+            body: formBody
+        }).then(response => response.json()).catch(error => {
+            console.log(error);
+        });
+    },
+    getPatientbyEmail(email){
+
+            return fetch(endpoint+'PatientByEmail/'+email, {
+                method: 'GET',
+                headers:this.loadHeaders(),
+            }).then(response => response.json()).catch(error => {
+                console.log(error);
+            });
+
+    },
+    getPatients(){
+
+        return fetch(endpoint+'Patients', {
+            method: 'GET',
+            headers:this.loadHeaders(),
+        }).then(response => response.json()).catch(error => {
+            console.log(error);
+        });
+
+    },
+    UpdatePatient(data) {
+        let formBody = [];
+        for (let property in data) {
+            let encodedKey = encodeURIComponent(property);
+            let encodedValue = encodeURIComponent(data[property]);
+            formBody.push(encodedKey + "=" + encodedValue);
+        }
+        formBody = formBody.join("&");
+        return fetch(endpoint + 'PatientUpdate', {
+            method: 'POST',
+            headers: this.loadHeaders(),
+            body: formBody
+        }).then(response => response.json()).catch(error => {
+            console.log(error);
+        });
+    },
+}
+
+
+export default PatientService;
--- Smartco/Webapps/ged.brainyfood/src/provider/webserviceQuestions.js
+++ Smartco/Webapps/ged.brainyfood/src/provider/webserviceQuestions.js
@@ -1,4 +1,4 @@
-const endpoint = "http://34.250.15.8/api/"
+const endpoint = process.env.REACT_APP_endpoint
 
 let QuestionService ={
 
