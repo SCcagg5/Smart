58147fd1d1c49900b49ca1135412f1725831a6b8
root <root@vps-dbc9edb5.vps.ovh.net>
2020-10-22 13:34:34 +0000

    fix

--- #Tool/Slave/Slave/cron/task/health/cron.py
+++ #Tool/Slave/Slave/cron/task/health/cron.py
@@ -1,7 +1,8 @@
 import time, sys, subprocess
-from elasticsearch import Elasticsearch
+#from elasticsearch import Elasticsearch
 import random
-
+import json
+import hashlib
 
 #es = Elasticsearch(["http://elasticsearch:9200"])
 
@@ -15,7 +16,25 @@ def ex(c):
 def main():
     dockers = ex("docker ps -a | tail -n $(($(docker ps -a | wc -l) - 1)) | rev | cut -d\" \" -f 1 | rev")
     for docker in dockers.split('\n'):
-        res = ex("docker inspect --format='{{json .State.Health}}' " + docker )
-        print(f"{docker} {res}")
+        res = json.loads(ex("docker inspect --format='{{json .State}}' " + docker ))
+        logs = res['Health']['Log'] if 'Health' in res else []
+        for log in logs:
+             id = hashlib.sha256((docker+log['Start']).encode('utf-8')).hexdigest()
+             log['Health'] = {}
+             log['Health']['Output'] = str(log['Output'].split('\n')[:-1])
+             log['Health']['ExitCode'] = log['ExitCode']
+             del log['Output']
+             del log['ExitCode']
+             log['Status'] = {}
+             log['Status']['Status'] = res['Status']
+             log['Status']['Running'] = res['Running']
+             log['Status']['Paused'] = res['Paused']
+             log['Status']['Restarting'] = res['Restarting']
+             log['Status']['Dead'] = res['Dead']
+             log['Status']['Error'] = res['Error']
+             log['Status']['StartedAt'] = res['StartedAt']
+             log['Status']['FinishedAt'] = res['FinishedAt'] if res['FinishedAt'] != "0001-01-01T00:00:00Z" else None
+             print(log)
+        #print(f"{docker} {res}")
 
 main()
