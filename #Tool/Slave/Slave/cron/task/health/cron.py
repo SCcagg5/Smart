7f5674c720925df27d9345338a2d58e0326f7ee9
SCcagg5 <eliot@LAPTOP-C7G3T243.localdomain>
2020-10-22 17:14:03 +0200

    update

--- #Tool/Slave/Slave/cron/task/health/cron.py
+++ #Tool/Slave/Slave/cron/task/health/cron.py
@@ -1,10 +1,11 @@
 import time, sys, subprocess
-#from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, helpers
+
 import random
 import json
 import hashlib
 
-#es = Elasticsearch(["http://elasticsearch:9200"])
+es = Elasticsearch(["http://elasticsearch:9200"])
 
 def ex(c):
     proc = subprocess.Popen([c], stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding = 'ascii', shell=True)
@@ -15,6 +16,13 @@ def ex(c):
 
 def main():
     dockers = ex("docker ps -a | tail -n $(($(docker ps -a | wc -l) - 1)) | rev | cut -d\" \" -f 1 | rev")
+    index = "service_health_" + date.today().strftime("%Y%m%d")
+    inputs = []
+    #if es.indices.exists(index=index):
+    #    es.indices.refresh(index=index)
+    #else:
+    #    es.indices.create(index=index, body=elastic.doc_mapping)
+    #    es.index(index=index, body=input, request_timeout=30)
     for docker in dockers.split('\n'):
         res = json.loads(ex("docker inspect --format='{{json .State}}' " + docker ))
         logs = res['Health']['Log'] if 'Health' in res else []
@@ -34,7 +42,14 @@ def main():
              log['Status']['Error'] = res['Error']
              log['Status']['StartedAt'] = res['StartedAt']
              log['Status']['FinishedAt'] = res['FinishedAt'] if res['FinishedAt'] != "0001-01-01T00:00:00Z" else None
-             print(log)
+             inputs.append({
+              "_index": index,
+              "_type": "_doc",
+              "_id": id,
+              "_score": 1,
+              "_source": log
+              })
+    helpers.bulk(es, inputs)
         #print(f"{docker} {res}")
 
 main()
