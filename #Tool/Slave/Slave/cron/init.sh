d7fe7d1b036ec39f38ffd25fa69fb56605a63558
SCcagg5 <eliot@LAPTOP-C7G3T243.localdomain>
2020-10-22 13:26:33 +0200

    fix

--- #Tool/Slave/Slave/cron/init.sh
+++ #Tool/Slave/Slave/cron/init.sh
@@ -1,22 +1,23 @@
 #/bin/bash
-echo -e "Starting"
+echo -e "$(date \"+[%Y-%m-%d %H:%M:%S]\")Starting"
+cron=""
 for i in $(ls -d */); do
 
   folder=${i%%/}
   CRON_SCHEDULE=$(printenv ${folder^^} || printenv ${folder})
-
   if [ -z "${CRON_SCHEDULE}" ]; then
-      >&2 echo -ne "Empty ${folder^^} environment variable\n"
+      >&2 echo -ne "$(date \"+[%Y-%m-%d %H:%M:%S]\") Empty ${folder^^} environment variable\n"
   else
-      mkdir "/home/cron/logs/$folder"
-      STDOUT_LOC="/home/cron/logs/$folder/log.suc"
-      STDERR_LOC="/home/cron/logs/$folder/log.err"
+      mkdir "/home/logs/$folder"
+      STDOUT_LOC="/home/logs/$folder/log.suc"
+      STDERR_LOC="/home/logs/$folder/log.err"
       tty=$(tty)
-      echo -ne "${CRON_SCHEDULE} echo \$(date \"+[%Y-%m-%d %H:%M:%S]\") \"Launching ${folder} process.\" > ${tty}\n" | crontab -
-      echo -ne "${CRON_SCHEDULE} /usr/local/bin/python3 /home/cron/${folder}/cron.py > ${STDOUT_LOC} 2> ${STDERR_LOC}\n" | crontab -
+
+      cron+="${CRON_SCHEDULE} echo \$(date \"+[%Y-%m-%d %H:%M:%S]\") \"Launching ${folder} process.\" > ${tty}\n"
+      cron+="${CRON_SCHEDULE} /usr/local/bin/python3 /home/cron/${folder}/cron.py > ${STDOUT_LOC} 2> ${STDERR_LOC}\n"
   fi
 
 done
-echo -e "Done\n"
-# run cron
+echo -ne "$cron" | crontab -
+echo -e "$(date \"+[%Y-%m-%d %H:%M:%S]\") Done\n"
 su -c "/usr/sbin/cron -f"
