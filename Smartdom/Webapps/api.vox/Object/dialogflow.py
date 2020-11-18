d672c0f3208cf5407e7abb3622c6c0f47e2def63
SCcagg5 <eliot@LAPTOP-C7G3T243.localdomain>
2020-11-18 22:24:38 +0100

    adding vox test

--- Smartdom/Webapps/api.vox/Object/dialogflow.py
+++ Smartdom/Webapps/api.vox/Object/dialogflow.py
@@ -1,6 +1,15 @@
 import json
-from dialogflow_fulfillment import WebhookClient
+from dialogflow_fulfillment import WebhookClient, QuickReplies
 
 class fulfillement:
+    def welcome(agent):
+         agent.add('How are you feeling today?')
+         agent.add(QuickReplies(quick_replies=['Happy :)', 'Sad :(']))
+
     def call(data):
-        return [True, data, None]
+        handler = {
+            "welcome": fulfillement.welcome
+        }
+        agent = WebhookClient(data)
+        agent.handle_request(handler[str(agent.intent)])
+        return [True, agent.response, None]
