8d3dab6e57d064900104e07e13ddda2bc804fa93
SCcagg5 <eliot@LAPTOP-C7G3T243.localdomain>
2020-11-19 00:29:02 +0100

    adding vox test

--- Smartdom/Webapps/api.vox/Object/dialogflow.py
+++ Smartdom/Webapps/api.vox/Object/dialogflow.py
@@ -7,8 +7,21 @@ class fulfillement:
          agent.add(QuickReplies(quick_replies=['Commander', 'Menu']))
 
     def command(agent):
-        agent.add('Bonjour, je suis Sacha l\'assistante virtuelle de commande, comment puis-je vous aider ?')
-        agent.add(QuickReplies(quick_replies=['Commander', 'Menu']))
+        if "product" not in agent.parameters:
+            agend.add("Que voulez vous commander ?")
+            return
+        prod = agent.parameters["product"]
+        if "number" not in agent.parameters:
+            nb = [1 for _ in prod]
+        else:
+            nb = agent.parameters["number"]
+            if len(nb) != len(prod):
+                agent.add("Pouvez-vous répeter en précisant la quantité pour chaque plat ?")
+                return
+        dprod = [("" if i == 1 else str(i) + " ") + prod[i].split('|')[0 if i == 1 else 1] for i in range(0, len(prod))]
+        talk_prod = (dprod[0] if len(dprod) == 1 else ", ".join(dprod[:-1]) + " et "+ dprod[len(dprod) - 1])
+        agent.add("Vous avez commandé " + talk_prod + ". C'est correct ?")
+        
 
     def error(agent):
         agent.add("Désolé, une erreur s'est produite")
