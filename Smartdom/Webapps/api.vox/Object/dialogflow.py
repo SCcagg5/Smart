ca5d6170c4439e66c63e7a80bd4ea2be66e80019
SCcagg5 <eliot@LAPTOP-C7G3T243.localdomain>
2020-11-18 23:57:49 +0100

    adding vox test

--- Smartdom/Webapps/api.vox/Object/dialogflow.py
+++ Smartdom/Webapps/api.vox/Object/dialogflow.py
@@ -3,15 +3,20 @@ from dialogflow_fulfillment import WebhookClient, QuickReplies
 
 class fulfillement:
     def welcome(agent):
-         agent.add('How are you feeling today?')
-         agent.add(QuickReplies(quick_replies=['Happy :)', 'Sad :(']))
+         agent.add('Bonjour, je suis Sacha l\'assistante virtuelle de commande, comment puis-je vous aider ?')
+         agent.add(QuickReplies(quick_replies=['Commander', 'Menu']))
+
+    def command(agent):
+        prod = agent.parameters["product"]
+        agent.add(f"Vous avez commandé { ", ".join(prod[:-1]) + ("" if len(prod) == 1 else f"et {prod[len(prod) - 1]}")}. C'est correct ?")
 
     def error(agent):
         agent.add("Désolé, une erreur s'est produite")
 
     def call(data):
         handler = {
-            "welcome": fulfillement.welcome
+            "welcome": fulfillement.welcome,
+            "Je voudrais commander": fulfillement.command
         }
         try:
             agent = WebhookClient(data)
